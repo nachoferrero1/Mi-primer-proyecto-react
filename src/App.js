fe6e4d6c067384c8c42e8edea09671139b2393ee
nachoferrero1 <nachoferreronf699@gmail.com>
2022-08-09 16:28:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from "./componentes/Testimonio.jsx"
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que nuestros alumnos dicen de nosotros</h1>
+        <Testimonio 
+        nombre='Bill Gates'
+        pais='Usa'
+        imagen='bill'
+        cargo='Ingeniero de Software'
+        empresa='Microsoft'
+        testimonio='Mi ambición ha sido siempre hacer realizables los sueños”
+        Los sueños, los deseos, son una fuente importante de motivación. Sin ellos, la voluntad puede quedar un poco ciega, desnortada, sin un camino claro hacia donde ir.
+        Para no dejarse arrastrar por la rutina diaria, conviene de vez en cuando plantearse objetivos. Y que éstos sean factibles, no demasiado elevados. Ir poco a poco hasta su consecución'
+        />
+        <Testimonio 
+        nombre='Mark Zuckerberg'
+        pais='Estados Unidos'
+        imagen='mark'
+        cargo='CEO'
+        empresa='Facebook'
+        testimonio='No tomamos una visión lo suficientemente amplia de nuestra responsabilidad, y eso fue un gran error", dirá Zuckerberg. "Fue un error mío, y lo siento. Comencé Facebook, lo administro y soy responsable de lo que sucede aquí'
+        />
+        <Testimonio 
+        nombre='Steve Jobs'
+        pais='USA'
+        imagen='steve'
+        cargo='CEO'
+        empresa='Apple'
+        testimonio='Tu trabajo va a llenar gran parte de tu vida, la única manera de estar realmente satisfecho es hacer lo que creas que es un gran trabajo y la única manera de hacerlo es amar lo que haces. '
+        />
+      </div>
     </div>
   );
 }
